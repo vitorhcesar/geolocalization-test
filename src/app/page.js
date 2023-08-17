4d84e8edff1d44f6a4cb4ccabb68bbfe354edc38
Vitor <thevitor.hugo27@gmail.com>
2023-08-17 07:15:55 -0300

    Quarto commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Map from "@/components/map";
+import dynamic from "next/dynamic";
+
+const MyMap = dynamic(() => import('../components/map'), { ssr: false });
 
 export default function App() {
 
     return (
         <div className="h-screen">
-            <Map /> 
+            <MyMap />
         </div>
     );
 }
