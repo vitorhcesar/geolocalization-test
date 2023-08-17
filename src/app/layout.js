7959a5370048cfe737c479e760df87ecd9c8d6ee
Vitor <thevitor.hugo27@gmail.com>
2023-08-16 23:06:53 -0300

    Segundo commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="pt-br">
             <head>
-                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+                <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossOrigin="true" />
             </head>
             <body>{children}</body>
         </html>
